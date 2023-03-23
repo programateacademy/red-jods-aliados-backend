7a5a07de5a563cbe136953de610e31558b2c73a8
Lesther1 <lesthercrespo@gmail.com>
2023-03-23 13:49:28 -0500

    documentacion v4 y role

--- Back/app/routes/nameroute.js
+++ Back/app/routes/nameroute.js
@@ -5,10 +5,10 @@ const checkRoleAuth = require('../middleware/roleAuth')
 const {getitems,getitem, newCharacter, editAlly, deleteAlly} = require('../controllers/namecontroller')
 const { cacheInit } = require('../middleware/cache')
 
-router.get('/', checkAuthtoken,checkRoleAuth(['admin']), cacheInit, getitems)
-router.get('/:id',checkAuthtoken,checkRoleAuth(['admin']),cacheInit, getitem)
-router.post('/',checkAuthtoken,checkRoleAuth(['admin']), newCharacter)
-router.put('/:id',checkAuthtoken,checkRoleAuth(['admin']), editAlly)
-router.delete('/:id', checkAuthtoken,checkRoleAuth(['admin']), deleteAlly)
+router.get('/', checkAuthtoken,checkRoleAuth(['admin','user']), cacheInit, getitems)
+router.get('/:id',checkAuthtoken,checkRoleAuth(['admin','user']),cacheInit, getitem)
+router.post('/',checkAuthtoken,checkRoleAuth(['admin','user']), newCharacter)
+router.put('/:id',checkAuthtoken,checkRoleAuth(['admin','user']), editAlly)
+router.delete('/:id', checkAuthtoken,checkRoleAuth(['admin','user']), deleteAlly)
 
 module.exports = router
